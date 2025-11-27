3bb747356cdc9de1d61f33d89b61f79e26f831ac
Константин-1122 <hamskijham331@gmail.com>
2025-11-27 20:24:24 +0300

    first commit

---
+++ main-3.js
@@ -0,0 +1,32 @@
+//5
+let a = Number(prompt(' вартість товару.'));
+let b = 2;
+let summa = a / b;
+alert(summa);
+
+
+//4
+let price = Number(prompt("ціна"));
+let quantity = Number(prompt('кількість'));
+let summaGoods = price * quantity;
+console.log(summaGoods);
+
+
+//3
+let uzerAge = Number(prompt("укажите возраст"));
+let age = uzerAge == 18;
+alert(age);
+
+
+//2
+let uzer1 = Number(prompt('введите число'));
+let uzer2 = uzer1 % 2 === 0;
+console.log(`четное ${uzer2}`);17
+
+//1
+let number1 = Number(prompt('введите число'));
+let number2 = Number(prompt('введите число'));
+console.log(`result ${number1 == number2}`);
+
+
+
